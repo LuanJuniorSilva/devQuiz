2b78cadf72d81bc6c50d80aa515849e488b7d456
luan jr <luan.juniorght7@gmail.com>
2024-06-17 02:27:53 -0300

    Init Project

--- android/app/src/main/java/com/devfitness2/MainApplication.kt
+++ android/app/src/main/java/com/devfitness2/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.devfitness2
+package com.devQuiz
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/devFitness2Tests/devFitness2Tests.m
+++ ios/devFitness2Tests/devFitness2Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface devFitness2Tests : XCTestCase
+@interface devQuizTests : XCTestCase
 
 @end
 
-@implementation devFitness2Tests
+@implementation devQuizTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
